1aeec259ce1af1899cb16b267fe7f0b656187e63
Joshua DeSantiago <joshuadesantiago2@gmail.com>
2018-05-15 12:19:49 -0400

    forgot to push

---
+++ include/Gregar_v.01/numfilm.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "film.h"
+#include "cnpy.h"
+
+#include <complex>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+
+class nfilm : public film {
+public:
+	nfilm();
+	nfilm(const unsigned int width, const unsigned int height);
+	~nfilm();
+	virtual void save(const std::string &filename, float *ptr);
+};
---
+++ src/film/numfilm.cpp
@@ -0,0 +1,16 @@
+#include "Gregar_v.01/numfilm.h"
+
+nfilm::nfilm()
+	:film(800,600){}
+
+nfilm::nfilm(const unsigned int width, const unsigned int height)
+	:film(width, height){}
+
+nfilm::~nfilm() {}
+
+void nfilm::save(const std::string &filename, float *ptr)
+{
+	std::cout << "Writing a " << mWidth << "x" << mHeight << " NPZ file to \"" << filename << "\"" << std::endl;
+
+	cnpy::npz_save(filename.c_str(), "radiances", ptr, { 3, mHeight, mWidth }, "w");
+}
