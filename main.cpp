78446cf33921687aba04ba632e6af76ad7aa6cbd
Joshua DeSantiago <joshuadesantiago2@gmail.com>
2018-05-31 15:14:50 -0400

    made a triangle and circle

--- main.cpp
+++ main.cpp
@@ -9,39 +9,100 @@
 #include "Gregar_v.01/ldrfilm.h"
 #define PI 3.14
 
-Eigen::Vector3d A(2,1,1);
-Eigen::Vector3d B(2, 0, -1);
-Eigen::Vector3d C(2, -1, 1);
-
-Eigen::Vector3d dr(1, 0, 0);
-Eigen::Vector3d P(0, 0, 0);
-
+Eigen::Vector3d A(-1, 1, -2);
+Eigen::Vector3d B(1, 1, -2);
+Eigen::Vector3d C(0, -1, -2);
 Eigen::Vector3d tri[3] = { A,B,C };
 
-int theta = 35;
+Eigen::Vector3d dr(0, 0, -1);
+Eigen::Vector3d P(0, 0, 0);
+
+Eigen::Vector3d R(-2, 1, -3);
+Eigen::Vector3d E(2, 1, -3);
+Eigen::Vector3d D(1, -1, -3);
+Eigen::Vector3d red[3] = { R,E,D };
+
+Eigen::Vector3d Q;
+
+int theta = 66;
+
+float tNearest = INFINITY;
+/////////////////////////////////////
+Eigen::Vector3d pt(0,0,-1);
+Eigen::Vector3d cen(0,0,-4);
+int radius = -2;
+
+int hit = 1;
+int miss = 0;
 
 int width = 800;
 int height = 600;
 
-int intersectiontest(Eigen::Vector3d p, Eigen::Vector3d dr, Eigen::Vector3d triangle[]) {
+int circletest(Eigen::Vector3d origin,Eigen::Vector3d dr, Eigen::Vector3d cen, int r) {
 
-	int hit = 1;
-	int miss = 0;
+	int rad = pow(r,2);
+	float Xo, Yo, Zo, Xd, Yd, Zd, Xc, Yc, Zc;
+	float t;
+
+	Xo = origin(0);
+	Yo = origin(1);
+	Zo = origin(2);
+
+	Xd = dr(0);
+	Yd = dr(1);
+	Zd = dr(2);
+
+	Xc = cen(0);
+	Yc = cen(1);
+	Zc = cen(2);
+	
+	float a = pow(Xd, 2) + pow(Yd, 2) + pow(Zd, 2);
+	float b = 2 * (((Xo*Xd)-(Xd*Xc))+ ((Yo*Yd) - (Yd*Yc))+ ((Zo*Zd) - (Zd*Zc)));
+	float c = (pow(Xo, 2) + pow(Xc, 2) - (2 * Xo*Xc))+ (pow(Yo, 2) + pow(Yc, 2) - (2 * Yo*Yc))+ (pow(Zo, 2) + pow(Zc, 2) - (2 *Zo*Zc))-rad;
+	
+	float D = pow(b, 2) - (4 * a * c);
+
+	if (D == 0) {
+		t = -b / 2;
+		return hit;
+	}
+	else if (D > 0) {
+		float t0 = (-b - pow(D, (1 / 2))) / (a * 2);
+		float t1 = (-b + pow(D, (1 / 2))) / (a * 2);
+		if (t0 > 0) {
+			t = t0;
+			return hit;
+		}
+		else
+			t = t1;
+		return hit;
+	}
+	else
+	{
+		return miss; 
+	}
+
+
+}
+
+int intersectiontest(Eigen::Vector3d p, Eigen::Vector3d dr, Eigen::Vector3d triangle[]) {
 
 	Eigen::Vector3d BA = triangle[1] - triangle[0];
 	Eigen::Vector3d CA = triangle[2] - triangle[0];
 	Eigen::Vector3d ABC = BA.cross(CA);
-	int CBA = ABC.norm();
+	float CBA = ABC.norm();
 
 	Eigen::Vector3d normal = ABC / CBA;
 
-	int d = normal.dot(triangle[0]);
+	float d = normal.dot(triangle[0]);
 
-	int t = (d - (normal.dot(p)))/(normal.dot(dr));
+	float t = (d - (normal.dot(p)))/(normal.dot(dr));
 
-	int check1 = ((triangle[1] - triangle[0]).cross(dr - triangle[0])).dot(normal);
-	int check2 = ((triangle[2] - triangle[1]).cross(dr - triangle[1])).dot(normal);
-	int check3 = ((triangle[0] - triangle[2]).cross(dr - triangle[2])).dot(normal);
+	Q = P + (t*dr);
+
+	float check1 = ((triangle[1] - triangle[0]).cross(Q - triangle[0])).dot(normal);
+	float check2 = ((triangle[2] - triangle[1]).cross(Q - triangle[1])).dot(normal);
+	float check3 = ((triangle[0] - triangle[2]).cross(Q - triangle[2])).dot(normal);
 
 	if (check1 >= 0 && check2 >= 0 && check3 >= 0)
 	{
@@ -50,6 +111,26 @@ int intersectiontest(Eigen::Vector3d p, Eigen::Vector3d dr, Eigen::Vector3d tria
 	else
 		return miss;
 }
+
+float intersectionloc(Eigen::Vector3d p, Eigen::Vector3d dr, Eigen::Vector3d triangle[]) {
+
+	int hit = 1;
+	int miss = 0;
+
+	Eigen::Vector3d BA = triangle[1] - triangle[0];
+	Eigen::Vector3d CA = triangle[2] - triangle[0];
+	Eigen::Vector3d ABC = BA.cross(CA);
+	float CBA = ABC.norm();
+
+	Eigen::Vector3d normal = ABC / CBA;
+
+	float d = normal.dot(triangle[0]);
+
+	float t = (d - (normal.dot(p))) / (normal.dot(dr));
+
+	return t;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -66,31 +147,84 @@ int main(int argc, char** argv)
 		std::cout << "Failed to allocate memory" << std::endl;
 	}
 
-
 	for (int i=0; i < height; i++){
 		for (int j=0; j < width; j++){
 
-			int Pcx = (AR*(2 * (j + .5 / width) - 1))*tan((theta/2)*PI/180);
-			int Pcy = (AR*(1-(i + .5 / height)*2))*tan((theta / 2)*PI / 180);
+			float Pcx = AR*(2 * ((j + .5) / width) - 1)*tan((theta/2)*PI/180);
+			float Pcy = AR*(1 - 2 * ((i + .5) / height))*tan((theta / 2)*PI / 180);
 
 			Eigen::Vector3d Pc(Pcx,Pcy,-1);
 			Eigen::Vector3d Rd = Pc - P;
 
-			int point = intersectiontest(P, Rd, tri);
+			float r = intersectionloc(P, Rd, red);
+			float w = intersectionloc(P, Rd, tri);
 
-			//std::cout << point << " ";
+			index = i * width * 4 + j * 4;
+			if (r < tNearest && w < tNearest && w < r) {
 
-			index = i* width * 4 + j * 4;
-			pixel[index + 0] = point; //Red
-			pixel[index + 1] = point; //Green
-			pixel[index + 2] = point; //Blue
-			pixel[index + 3] = 1.0; // Alpha
+				float point = intersectiontest(P, Rd, tri);
+
+				//std::cout << point << " ";
+
+				pixel[index + 0] = point; //Red
+				pixel[index + 1] = point; //Green
+				pixel[index + 2] = point; //Blue
+				pixel[index + 3] = 1.0; // Alpha
+			}
+			else {
+
+				float point = intersectiontest(P, Rd, red);
+
+				//std::cout << point << " ";
+
+				pixel[index + 0] = point; //Red
+				pixel[index + 1] = 0; //Green
+				pixel[index + 2] = 0; //Blue
+				pixel[index + 3] = 1.0; // Alpha
+
+			}
 		}
 		//std::cout << std::endl;
 	}
 
-	test_ldr.save("A.png", pixel);
-	std::cout << "Here";
+	//test_ldr.save("A.png", pixel);
+	//std::cout << "Here";
 
 	//int point = intersectiontest(P,dr,tri);
+////////////////////////////////////////////////////////////////////////////////////
+	float *pix = new float[size];
+	if (pix == NULL)
+	{
+		std::cout << "Failed to allocate memory" << std::endl;
+	}
+
+	//int mark = circletest(P,pt,cen,radius);
+	//std::cout<< mark << std::endl;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+
+			float Pcx = AR * (2 * ((j + .5) / width) - 1)*tan((theta / 2)*PI / 180);
+			float Pcy = AR * (1 - 2 * ((i + .5) / height))*tan((theta / 2)*PI / 180);
+
+			Eigen::Vector3d Pc(Pcx, Pcy, -1);
+			Eigen::Vector3d rd = Pc - P;
+
+			int mark = circletest(P, rd, cen, radius);
+
+			index = i * width * 4 + j * 4;
+
+				//std::cout << mark << " ";
+
+				pix[index + 0] = mark; //Red
+				pix[index + 1] = mark; //Green
+				pix[index + 2] = mark; //Blue
+				pix[index + 3] = 1.0; // Alpha
+
+		}
+		//std::cout << std::endl;
+	}
+
+	test_ldr.save("A.png", pix);
+	std::cout << "Here";
 }
